2660addd16e4cc940fd6f486431e84308ff06bb2
Satoru Komatsu <skomatsu@c3r3s5.42tokyo.jp>
2025-10-16 22:22:13 +0900

    add header

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,10 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skomatsu <skomatsu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 21:25:51 by skomatsu          #+#    #+#             */
+/*   Updated: 2025/10/16 21:33:02 by skomatsu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
 
 int main (void)
 {
     Fixed a;
+    Fixed b(a);
+    Fixed c;
+    
+    c = b;
     
     std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    
     a.setRawBits(42);
     std::cout << a.getRawBits() << std::endl;
     return (0);
